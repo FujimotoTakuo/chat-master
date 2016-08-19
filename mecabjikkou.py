21eeb612005671edecdbf36c9b05e99a9f6711a5
FujimotoTakuo <takuo-fujimoto@to-be.co.jp>
2016-08-19 22:39:31 +0900

    ww
    weqw

--- mecabjikkou.py
+++ mecabjikkou.py
@@ -5,4 +5,15 @@ import MeCab
 ### Execute
 if __name__ == "__main__":
     print(u'Main run')
+    param = sys.argv
+    path = param[1]
+    out_path = path + '_mecabed.txt'
+    in_file = open(path, 'r')
+    out_file = open(out_path, 'w')
     
+    m = MeCab.Tagger ("-Owakati")
+    for line in in_file:
+        result = tagger.parse(line)
+        out_file.write(result)
+    
+
