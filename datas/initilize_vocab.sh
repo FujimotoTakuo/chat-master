7cfd7d5e1a40fc046b3b0328594de5f76d82f662
FujimotoTakuo <dev10635gce004@instance-1.c.true-gradient-141000.internal>
2016-08-22 03:12:06 +0000

    ssa

---
+++ datas/initilize_vocab.sh
@@ -0,0 +1,10 @@
+#/bin/sh
+
+cat /dev/null > test_data_ids_out.txt
+cat /dev/null > test_data_ids_in.txt
+cat /dev/null > train_data_ids_out.txt
+cat /dev/null > train_data_ids_in.txt
+cat /dev/null > vocab_in.txt
+cat /dev/null > vocab_out.txt
+
+
