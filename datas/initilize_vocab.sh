32816b85f4de731f5e8eae74b0818f901f5aeb23
FujimotoTakuo <takuo-fujimoto@to-be.co.jp>
2016-08-22 16:27:33 +0900

    dsffsfds
    fsd

--- datas/initilize_vocab.sh
+++ datas/initilize_vocab.sh
@@ -1,10 +1,12 @@
 #/bin/sh
 
-cat /dev/null > test_data_ids_out.txt
-cat /dev/null > test_data_ids_in.txt
-cat /dev/null > train_data_ids_out.txt
-cat /dev/null > train_data_ids_in.txt
-cat /dev/null > vocab_in.txt
-cat /dev/null > vocab_out.txt
+datas=dirname $0
+
+cat /dev/null > $datas/test_data_ids_out.txt
+cat /dev/null > $datas/test_data_ids_in.txt
+cat /dev/null > $datas/train_data_ids_out.txt
+cat /dev/null > $datas/train_data_ids_in.txt
+cat /dev/null > $datas/vocab_in.txt
+cat /dev/null > $datas/vocab_out.txt
 
 
