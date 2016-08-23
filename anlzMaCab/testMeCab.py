ecde90900a79f39e274e7336ffef9eb7c04e210c
FujimotoTakuo <takuo-fujimoto@to-be.co.jp>
2016-08-23 15:17:59 +0900

    sad
    dsa

--- anlzMaCab/testMeCab.py
+++ anlzMaCab/testMeCab.py
@@ -28,9 +28,9 @@ if __name__ == "__main__":
               hinshiMap[hinshi] = list
             result_node = result_node.next
     
-    all_file = open('all.txt')
+    all_file = open('all.txt', 'w')
     for  (hinshi, text_list) in hinshiMap.iteritems():
-        write_file = open(hinshi + '.txt', w)
+        write_file = open(hinshi + '.txt', 'w')
         for text in text_list:
             write_file.write(text + "\n")
             all_file.write(hinshi + ',' + text + "\n")
