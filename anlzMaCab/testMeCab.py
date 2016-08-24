fce7947b8c4854fff754696465e33fa0e6c3eae8
takuo-fujimoto <takuo-fujimoto@to-be.co.jp>
2016-08-24 12:00:33 +0900

    k

--- anlzMaCab/testMeCab.py
+++ anlzMaCab/testMeCab.py
@@ -11,11 +11,11 @@ if __name__ == "__main__":
     out_path = path + '_mecabed.txt'
     in_file = open(path, 'r')
     out_file = open(out_path, 'w')
-    
+
     m = MeCab.Tagger ("-Ochasen")
-    
+
     hinshiMap = {}
-    
+
     kuttuke_list = ["名詞-and-接尾-and-一般-and-*",
                     "助詞-and-終助詞-and-*-and-*",
                     "名詞-and-接尾-and-助数詞-and-*",
@@ -35,12 +35,37 @@ if __name__ == "__main__":
                     "名詞-and-接尾-and-助数詞-and-*",
                     "名詞-and-接尾-and-助動詞語幹-and-*",
                     "名詞-and-接尾-and-副詞可能-and-*"]
-    
+
+        kuttuke_word_list = ["されている", "いただける", "しよう", "する", "させていただき", "しました",
+                             "した", "します", "したい", "していくより", "していきたい", "して",
+                             "いただけると", "し", "している", "ある", "して行くと", "してくれる",
+                             "しょう", "したら", "しています", "してください", "しない", "いる",
+                             "できないですし", "してしまいます", "したくても", "いるので", "つき", "せんでした",
+                             "していて", "なり", "された", "される", "いません", "すればいい",
+                             "できました", "いました", "すると", "させる", "するし", "してます",
+                             "しても", "していた", "すれば", "なってしまいそうで", "していなかっても", "していなくっても",
+                             "させて頂いております", "させていただいております", "させていただきます", "しましますが", "しまいますが", "しそうに",
+                             "していました", "しやすい", "してて", "しましたが", "しておくと", "するよ",
+                             "してみた", "されなかった", "していますが", "しれませんが", "しており", "しております",
+                             "するので", "されているので", "してる", "できるでしょう", "しておきます", "しちゃってる",
+                             "するなら", "してたら", "し始め", "しろ", "してから", "されてき",
+                             "されない", "しますが", "してくれました", "されます", "されてしまいました", "されましたが",
+                             "してますが", "しておらず", "されるものの", "しないといけない", "してありました", "してない",
+                             "したいです", "しても良い", "してもよい", "してもいい", "されず", "しているので",
+                             "していないので", "しないので", "してくれたら", "しなくてはなりません", "しておりました", "しつつ",
+                             "していたので", "しそうなので", "してしまい", "したので", "して下さい", "してください",
+                             "してきましたが", "したくないので", "してもらう", "さしたら", "させたら", "させて下さい",
+                             "させてください", "し終えた", "しおえた", "しておけば", "したが", "して欲しいです",
+                             "してほしいです", "していく", "していると", "しなくて", "してしまう", "してい",
+                             "しながら", "しないのに", "してくる", "してあり", "しにくく", "しませんでした",
+                             "してた", "してある", "させていこう", "させていただこう", "してれば", "していれば",
+                             "しておりません", "しなければならない", "しないで"]
+
     atama_list = ["接頭詞-and-名詞接続-and-*-and-*"]
-    
+
     hajikknai_kigou_list = ["記号-and-句点-and-*-and-*", "記号-and-読点-and-*-and-*"]
-    
-    
+
+
     # 頭にくっつけたり、後ろにくっつけたりする必要がある
     # そのまま出力はしない。くっつけの判定を終えて確定した前テキストから出力をする
     # まず、1行目は、前テキストに保持して終わり
@@ -50,8 +75,8 @@ if __name__ == "__main__":
     #   ⇒  1 2は同時に出ないはず
     #   1, 2 にも該当しない場合 : 前テキストは、確定した。出力する。出力後、前テキスト情報に今テキスト情報を入れる
     # ループ終了 : 最終行が前テキストに入っているので、出力
-    # 
-    
+    #
+
     for line in in_file:
         result_node = m.parseToNode(line)
         before_text = ""
@@ -69,13 +94,13 @@ if __name__ == "__main__":
                 before_hinshi = hinshi
                 result_node = result_node.next
                 continue
-            
+
             # 記号は基本はじくが、句読点ははじかない。
             # はじかないListに含まれない、記号はcontinue
             if '記号' in split_array[0] and not hinshi in hajikknai_kigou_list:
                 result_node = result_node.next
                 continue
-            
+
             # 前のテキストが、頭に付けるかチェック
             if before_hinshi in atama_list:
                 # 前のテキスト + 今のテキスト
@@ -84,16 +109,16 @@ if __name__ == "__main__":
                 before_hinshi = hinshi
                 result_node = result_node.next
                 continue
-            
+
             # 今のテキストが、後ろにくっつける品詞かチェック
-            if hinshi in kuttuke_list:
+            if hinshi in kuttuke_list or current_text in kuttuke_word_list:
                 # 前のテキスト + 今のテキスト　まだ単語の確定はしない before_hinshiは変えない。
                 before_text = before_text + current_text
                 result_node = result_node.next
                 continue
-            
+
             # before を書き込む
-            
+
             if before_hinshi in hinshiMap:
                 list = hinshiMap[before_hinshi]
                 if not before_text in list:
@@ -104,9 +129,9 @@ if __name__ == "__main__":
             # 今のテキストは保持して次へ
             before_text = current_text
             before_hinshi = hinshi
-            
+
             result_node = result_node.next
-    
+
         if before_hinshi in hinshiMap:
             list = hinshiMap[before_hinshi]
             if not before_text in list:
@@ -114,18 +139,14 @@ if __name__ == "__main__":
         else :
             list = [before_text]
             hinshiMap[before_hinshi] = list
-    
+
     all_file = open(out_path, 'w')
     for  (hinshi, text_list) in hinshiMap.iteritems():
         print("write : " + hinshi + '.txt' + " / count : " + str(len(text_list)))
-        # write_file = open(hinshi + '.txt', 'w')
+        write_file = open(hinshi + '.txt', 'w')
         for text in text_list:
-            # write_file.write(text + "\n")
+            write_file.write(text + "\n")
             all_file.write(hinshi + ',' + text + "\n")
-        # write_file.close()
-    
+        write_file.close()
+
     all_file.close()
-
-
-
-
