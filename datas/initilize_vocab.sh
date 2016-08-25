75495213667af5121cd3b23d1a619db1fad57b80
takuo-fujimoto <takuo-fujimoto@to-be.co.jp>
2016-08-25 19:32:17 +0900

    k

--- datas/initilize_vocab.sh
+++ datas/initilize_vocab.sh
@@ -4,6 +4,8 @@ moto=`pwd`
 cd $(dirname $0)
 datas=`pwd`
 
+rm $datas/*.txt
+
 cat /dev/null > $datas/test_data_ids_out.txt
 cat /dev/null > $datas/test_data_ids_in.txt
 cat /dev/null > $datas/train_data_ids_out.txt
@@ -12,4 +14,3 @@ cat /dev/null > $datas/vocab_in.txt
 cat /dev/null > $datas/vocab_out.txt
 
 cd $moto
-
